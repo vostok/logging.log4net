893a2ac7e1f8910ed93871075fb6afd104d8b04a
iloktionov <iloktionov@skbkontur.ru>
2018-08-17 13:19:40 +0500

    Log4netLog: xml-docs.

--- Vostok.Logging.Log4net/Log4netLog.cs
+++ Vostok.Logging.Log4net/Log4netLog.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using JetBrains.Annotations;
+using log4net;
 using log4net.Core;
+using log4net.Repository;
 using log4net.Repository.Hierarchy;
 using Vostok.Logging.Abstractions;
 using ILog = Vostok.Logging.Abstractions.ILog;
 
 namespace Vostok.Logging.Log4net
 {
-    // TODO(iloktionov): 1. xml-docs
     // TODO(iloktionov): 2. better unit test coverage (ForContext)
     // TODO(iloktionov): 3. do something about global properties (log4net:HostName, log4net:UserName, log4net:Identity)
 
+    /// <summary>
+    /// <para>Represents an adapter between Vostok logging interfaces and log4net.</para>
+    /// <para>It implements Vostok <see cref="ILog"/> interface using an externally provided instance of log4net <see cref="ILogger"/>.</para>
+    /// <para>It does this by following these rules:</para>
+    /// <list type="number">
+    ///     <item><description>Vostok <see cref="LogLevel"/>s are directly translated to log4net <see cref="Level"/>s.<para/></description></item>
+    ///     <item><description>Messages are prerendered into text as Vostok <see cref="ILog"/>'s formatting syntax differs from log4net.<para/></description></item>
+    ///     <item><description>Properties are forwarded into log4net event's <see cref="LoggingEvent.Properties"/>.<para/></description></item>
+    ///     <item><description><see cref="ForContext"/> with <c>null</c> argument returns a <see cref="Log4netLog"/> based on root logger from log4net's repository.<para/></description></item>
+    ///     <item><description><see cref="ForContext"/> with non-<c>null</c> argument returns a <see cref="Log4netLog"/> based on logger with name equal to context value, obtained with <see cref="ILoggerRepository.GetLogger"/>.<para/></description></item>
+    ///     <item><description><para/></description></item>
+    /// </list>
+    /// </summary>
     public class Log4netLog : ILog
     {
         private readonly ILogger logger;
